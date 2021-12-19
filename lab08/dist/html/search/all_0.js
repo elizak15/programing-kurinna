9519c385b249a5199821786ddfa7ada57ca1c4ca
elizak15 <Yelyzaveta.Kurinna@cit.khpi.edu.ua>
2021-12-19 11:44:33 +0200

    Deleted error

--- lab08/dist/html/search/all_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['main_0',['main',['../main_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.c']]],
-  ['main_2ec_1',['main.c',['../main_8c.html',1,'']]],
-  ['matrix_2',['matrix',['../main_8c.html#a623a8bc04a7b13ca7443ce612045903f',1,'main.c']]]
-];
--- lab08/dist/html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['truefalse_5fwhile_8',['truefalse_while',['../main_8c.html#a910cc219502eee533796f15f949f5b04',1,'main.c']]]
-];
