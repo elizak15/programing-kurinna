69837553dbcf299833aec16da4e386e0cfc94e39
elizak15 <Yelyzaveta.Kurinna@cit.khpi.edu.ua>
2022-04-05 22:53:39 +0300

    Added file with library

---
+++ lab10/src/lib.h
@@ -0,0 +1,18 @@
+#ifndef __LIB1_H__
+#define __LIB1_H__
+/**
+ * @file lib.h
+ * @brief Файл з функцією із завданням визначити найбільшу суму непреривного інтервалу з позитивних чисел.
+ * @author Kurinna E.
+ * @date 5-apr-2022
+ * @version 1.3
+ **/
+
+/** Визначення найбільшої суми непреривного інтервалу позитивних чисел
+ *@param array1 вхідний масив
+ *@param n розмір масиву
+ *return повертає значення result.
+ **/
+int find_maximum(int *array1, int n);
+
+#endif
